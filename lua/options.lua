bbbf419f21b1821b09abd9cc6e9a26fcbc06e504
tyPhoon-collab <typhoonmoon1022@gmail.com>
2026-01-01 05:58:00 +0000

    feat: SSH時のOSC 52の設定を追加

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,18 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- SSH接続時にOSC 52クリップボードを強制有効化
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
